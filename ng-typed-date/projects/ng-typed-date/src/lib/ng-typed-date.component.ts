ab473e405e215a3aeb1e712b40936cb6e12c1e64
Renan Aragão <refaragao@gmail.com>
2021-05-17 17:48:25 -0300

    create componente typed date

---
+++ ng-typed-date/projects/ng-typed-date/src/lib/ng-typed-date.component.ts
@@ -0,0 +1,71 @@
+import { Component, forwardRef, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'ng-typed-date',
+  template: `
+    <input
+      type="date"
+      id="date"
+      name="date"
+      [min]="min"
+      [max]="min"
+      (change)="onChangeDate($event.target.value)"
+      [ngModel]="date | date:'yyyy-MM-dd'"
+      (ngModelChange)="date = $event"
+      [ngModelOptions]="{updateOn: 'blur'}"
+      [required]="required"
+      [disabled]="disabled">
+  `,
+  styles: [
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NgTypedDateComponent),
+      multi: true
+    }
+  ]
+})
+export class NgTypedDateComponent implements OnInit {
+  private _date: Date;
+
+  public get date(): Date {
+    return this._date;
+  }
+  public set date(value: Date) {
+    this._date = value;
+    this.onChange(this._date);
+  }
+  min: Date;
+  max: Date;
+  required: boolean;
+  disabled: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onChangeDate(event: string) {
+    const [year, month, day] = event.split('-');
+    this.date = new Date(Number(year), Number(month) - 1, Number(day));
+  }
+
+  writeValue(obj: any): void {
+    this.date = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onChange: any = () => { };
+  onTouch: any = () => { };
+
+}
---
+++ ng-typed-date/projects/ng-typed-date/src/lib/ng-typed-date.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgTypedDateComponent } from './ng-typed-date.component';
+
+
+
+@NgModule({
+  declarations: [NgTypedDateComponent],
+  imports: [
+  ],
+  exports: [NgTypedDateComponent]
+})
+export class NgTypedDateModule { }
