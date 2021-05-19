adc95c4fa7ef09cea334ce00ffe99259c0dd1f9a
Renan Aragão <refaragao@gmail.com>
2021-05-19 14:41:49 -0300

    create directive ngModelDate

---
+++ ng-typed-date/projects/ng-typed-date/src/lib/ng-model-date.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NgModelDateDirective } from './ng-model-date.directive';
+
+describe('NgModelDateDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NgModelDateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
