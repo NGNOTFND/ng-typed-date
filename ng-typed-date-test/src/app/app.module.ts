0631cf31fb4f67aae1323d47769dedf0f8f0c223
Renan Teixeira <renan_replay@live.com>
2021-06-11 15:47:19 -0300

    bug fix to date entry works correctly

--- ng-typed-date-test/src/app/app.module.ts
+++ ng-typed-date-test/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { NgModelDateDirective } from './ng-model-date.directive';
+
 
 
 @NgModule({
